eccd7025a404dbe02fb8d48b2896981a752885b2
dtrift <89174579997@mail.ru>
2019-09-01 20:48:26 +0500

    lesson_3_5

--- db/migrate/20190831175351_add_tests_level_default_constraint.rb
+++ db/migrate/20190831175351_add_tests_level_default_constraint.rb
@@ -1,5 +1,5 @@
 class AddTestsLevelDefaultConstraint < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :tests, :level, from: nil, to: false
+    change_column_default :tests, :level, from: nil, to: 0
   end
 end
