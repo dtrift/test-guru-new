af2611c8e2b475749f1bf03fed026f7a091a6253
dtrift <89174579997@mail.ru>
2019-09-23 22:34:37 +0500

    Lesson 8.4 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[index show edit update destroy]
-  before_action :find_test, only: %i[new create destroy]
+  before_action :find_question, only: %i[show edit update]
+  before_action :find_test, only: %i[new create update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -8,9 +8,7 @@ class QuestionsController < ApplicationController
     @questions = @test.questions.all
   end
 
-  def show
-    # @question_answers = @qustion.answers
-  end
+  def show; end
 
   def new
     @question = Question.new
@@ -48,7 +46,7 @@ class QuestionsController < ApplicationController
   end
 
   def find_test
-    @test = Test.find(params[:id])
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
