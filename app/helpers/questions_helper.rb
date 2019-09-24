8a77d40a8aa0aa3fd842a323326223521e1438a5
dtrift <89174579997@mail.ru>
2019-09-24 20:56:29 +0500

    Lesson 8.4 6

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def question_header(question)
+    test_title = question.test.title
+    question.new_record? ? "Create new #{test_title} question" : "Edit #{test_title} question"
+  end
 end
