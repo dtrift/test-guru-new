3857435e528f4f7129333ed7d6ff55089fafd03a
dtrift <89174579997@mail.ru>
2019-09-26 18:52:08 +0500

    Lesson 9 Answers Completed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :count, on: :create
+  validate :count_answers, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
   
-  def count
-    errors.add(:category_id, 'Количество ответов для одного вопроса от 1 до 4') if question.answers.count >= 4
+  def count_answers
+    errors.add(:question_id, 'Количество ответов для одного вопроса от 1 до 4') if question.answers.count >= 4
   end
 end
