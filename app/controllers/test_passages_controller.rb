d8d8449b60449c8942e18a137984043521d8bd9b
dtrift <89174579997@mail.ru>
2019-09-30 22:20:36 +0500

    Lesson 9 Result comleted

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,13 +1,9 @@
 class TestPassagesController < ApplicationController
   before_action :find_test_passage, only: %i[show update result]
 
-  def show
-    
-  end
+  def show; end
 
-  def result
-    
-  end
+  def result; end
 
   def update
     @test_passage.accept!(params[:answer_ids])
@@ -17,10 +13,6 @@ class TestPassagesController < ApplicationController
     else
       render :show
     end
-  end
-
-  def one
-    @one = 1
   end
 
   private
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,10 +1,12 @@
 class TestPassage < ApplicationRecord
+  POSITIVE_SCORE = 85.freeze
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_next_question, on: :update
+  before_update :before_update_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
@@ -18,23 +20,37 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def index_current_question
+    test.questions.index(current_question) + 1
+  end
+
+  def score
+    (100 / test.questions.count) * correct_questions
+  end
+
+  def score_positive?
+    score >= 85
+  end
+
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
   end
 
-  def before_update_next_question
+  def before_update_set_next_question
     self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
   end
+
+  # def score
+  #   (100 / test.questions.count) * correct_questions
+  # end
 end
