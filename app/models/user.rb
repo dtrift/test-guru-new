3eed0192d7005b26236dff6c894c5c9c65721ebb
dtrift <89174579997@mail.ru>
2019-10-03 22:19:58 +0500

    Lesson 10 Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+  include Auth
+
+  has_secure_password
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :user_id
-
-  validates :name, :email, presence: true
-  validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
 
   def all_tests(level_test)
     tests.where(level: level_test)
