0a7d3a9a2bf954de70722bdf62d2e707c854dc18
dtrift <89174579997@mail.ru>
2019-10-06 22:14:17 +0500

    Add flash helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   validates :name, :email, presence: true 
   validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :email, uniqueness: true
 
   has_secure_password
 
