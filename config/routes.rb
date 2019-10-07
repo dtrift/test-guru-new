cb42179ff936de4dd68486d71e2f6c7f71268577
dtrift <89174579997@mail.ru>
2019-10-07 19:07:35 +0500

    Correct logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
-  get :logout, to: 'sessions#delete'
+  delete :logout, to: 'sessions#delete'
 
   resources :users, only: :create
   resources :sessions, only: :create
