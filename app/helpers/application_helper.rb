a1310587ac81eec3efa966221857724ea85cc338
dtrift <89174579997@mail.ru>
2019-10-18 06:26:23 +0500

    Edit flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+  FLASH_TYPES = {
+    notice: :primary,
+    alert: :secondary,
+    error: :danger
+  }
+
   def current_date
     Date.current
   end
 
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
+  end
+
+  def flash_type(type)
+    FLASH_TYPES[type.to_sym] || FLASH_TYPES[:notice]
   end
 end
